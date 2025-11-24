372e09cceaedd0d17b68a14bfb3ce73995176d34
Manas Jiwnani <Manasjiw26@gmail.com>
2025-11-24 15:18:40 +0530

    testing

---
+++ Project LOVE/ActivityCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityCollectionViewCell.swift
+//  Project LOVE
+//
+//  Created by SDC-USER on 24/11/25.
+//
+
+import UIKit
+
+class ActivityCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Project LOVE/DataSource.swift
@@ -0,0 +1,8 @@
+//
+//  DataSource.swift
+//  Project LOVE
+//
+//  Created by SDC-USER on 24/11/25.
+//
+
+import Foundation
