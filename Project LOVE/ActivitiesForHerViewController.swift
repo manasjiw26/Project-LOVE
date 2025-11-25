a6a7ea84d00c3347a26a94790e6bdbf1e4bbe299
Manas Jiwnani <Manasjiw26@gmail.com>
2025-11-25 16:45:33 +0530

    Activities for her completed,App bacground set,

--- Project LOVE/ActivitiesForHerViewController.swift
+++ Project LOVE/ActivitiesForHerViewController.swift
@@ -6,24 +6,51 @@
 //
 
 import UIKit
+import Foundation
 
-class ActivitiesForHerViewController: UIViewController {
-
+class ActivitiesForHerViewController: UIViewController, UICollectionViewDelegate {
+    
+    @IBOutlet weak var activityCollectionView: UICollectionView!
+    
+    var activitiesForHer = dataStore.getActivities()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("Count:", activitiesForHer.count)
         // Do any additional setup after loading the view.
+        activityCollectionView.dataSource = self
+        activityCollectionView.delegate = self
+//        if let layout = activityCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+//            layout.itemSize = CGSize(width: view.bounds.width - 32, height: 120)
+//            layout.minimumLineSpacing = 12
+//            layout.sectionInset = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+//        }
+        registerCells()
     }
 
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func registerCells() {
+        activityCollectionView.register(UINib(nibName: "ActivityCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "activity_cell")
     }
-    */
+}
 
+
+
+extension ActivitiesForHerViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return activitiesForHer.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "activity_cell", for: indexPath) as! ActivityCollectionViewCell
+        cell.configureCells(activity: activitiesForHer[indexPath.row])
+        return cell
+    }
+}
+extension ActivitiesForHerViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let padding: CGFloat = 32 // 16 left + 16 right
+        let width = collectionView.bounds.width - padding
+        let height: CGFloat = 115 // Adjust this based on your cell's content
+        return CGSize(width: width, height: height)
+    }
 }
--- Project LOVE/Model/DataStore.swift
+++ Project LOVE/Model/DataStore.swift
@@ -14,14 +14,22 @@ class DataStore {
     }
     func loadSampleData() {
         var sampleActivities: [Activity] = [
-            Activity(name: "Chill & Glow Sesh", description: "Facemasks, candles, chill beats — just cozy vibes and glow time ", image: "", time: "5 mins"),
+            Activity(name: "Chill & Glow Sesh", description: "Facemasks, candles, chill beats — just cozy vibes and glow time ", image: "Activityimage", time: "5 mins"),
             Activity(name: "Petal Hunt", description: "Pick pretty blooms and build your own bouquet together.", image: "Activityimage", time: "5 mins"),
             Activity(name: "Wholesome Craft Challenge", description: "Make a doodle / note / digital collage for her", image: "Activityimage", time: "5 mins"),
+            Activity(name: "Memory Lane Marathon", description: "Make a mini reel using your photos and favorite audio", image: "Activityimage", time: "5 mins"),
+            Activity(name: "Stuff-A-Memory Day", description: "Buy a tine plush, both name it and take care of it together", image: "Activityimage", time: "5 mins"),
+            Activity(name: "Memory Lane Marathon", description: "Make a mini reel using your photos and favorite audio", image: "Activityimage", time: "5 mins"),
             Activity(name: "Memory Lane Marathon", description: "Make a mini reel using your photos and favorite audio", image: "Activityimage", time: "5 mins"),
             Activity(name: "Stuff-A-Memory Day", description: "Buy a tine plush, both name it and take care of it together", image: "Activityimage", time: "5 mins"),
             Activity(name: "Memory Lane Marathon", description: "Make a mini reel using your photos and favorite audio", image: "Activityimage", time: "5 mins")]
         
         self.activities = sampleActivities
     }
-    var dataStore = DataStore()
+    func getActivities() -> [Activity] {
+        return activities
+    }
+    
+    
 }
+var dataStore = DataStore()
